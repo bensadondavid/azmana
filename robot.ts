a41c54aa377fd58b57cc7cc5ed9889c48665135e
David <bensadondavidn@gmai.com>
2026-04-16 00:33:46 +0300

    changes

---
+++ robot.ts
@@ -0,0 +1,10 @@
+// app/robots.ts
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://azmana.fr/sitemap.xml",
+  };
+}
---
+++ sitemap.ts
@@ -0,0 +1,10 @@
+// app/sitemap.ts
+
+export default function sitemap() {
+  return [
+    {
+      url: "https://azmana.fr",
+      lastModified: new Date(),
+    },
+  ];
+}
